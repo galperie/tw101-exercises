ff879755bb2f152fde9b5f11d6455c1b9a59ed27
Ellen Galperin <galperie@lafayette.edu>
2015-06-01 14:54:05 -0400

    Finished first exercise. May have to fix if need to do TDD.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -5,12 +5,13 @@ package com.thoughtworks.tw101.exercises.exercise1;
 public class Main {
 
     public static int SUM = 0;
+    public static int MAXNUM = 100;
 
     public static void main(String[] args) {
         Main main = new Main();
 
-        for(int i = 1; i <= 100; i++ ) {
-            if(main.isOdd(i) != 0) {
+        for(int i = 1; i <= MAXNUM; i++ ) {
+            if(main.isOdd(i)) {
                 main.sumOfNumbers(i);
                 main.printNumber(i);
             }
@@ -23,8 +24,8 @@ public class Main {
         System.out.print("\n");
     }
 
-    public int isOdd(int x) {
-        return x % 2;
+    public boolean isOdd(int x) {
+        return x % 2 != 0;
     }
 
     private void sumOfNumbers(int x) {
