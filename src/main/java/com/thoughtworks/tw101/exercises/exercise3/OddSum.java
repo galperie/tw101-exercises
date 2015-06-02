453c4b51ba519f6f22c0ee12a28e952a2c9be8fd
Ellen Galperin <galperie@lafayette.edu>
2015-06-02 17:52:33 -0400

    Finished third exercise

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,16 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int sum = 0;
+
+        for(int i = start; i <= end; i++) {
+            if(isOdd(i))
+                sum += i;
+        }
+        return sum;
+    }
+
+    private boolean isOdd(int x) {
+        return x % 2 != 0;
     }
 }
